23480bfce318da4afb9d1de944bbeb284456bf2c
Harpreet Sangar <harpreet.sangar@applify.co>
2022-04-27 17:52:22 +0530

    refactor: Make firebase storage instance private.

--- lib/services/firebase_storage_service.dart
+++ lib/services/firebase_storage_service.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseStorageService {
-  final FirebaseStorage storage = FirebaseStorage.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   final String _imagePath = 'chat_uploads/';
   final String _documentPath = 'chat_uploads/files/';
@@ -20,12 +20,25 @@ class FirebaseStorageService {
   }) =>
       _uploadFile(file: file, messageId: messageId, path: _documentPath);
 
+  Future<String?> uploadDocFile(File? filePath, String messageId) async {
+    String? url;
+    try {
+      final Reference reference =
+          _storage.ref("chat_uploads/files/$messageId.pdf");
+      await reference.putData(filePath!.readAsBytesSync());
+      url = await reference.getDownloadURL();
+    } on FirebaseException catch (e) {
+      print(e);
+    }
+    return url;
+  }
+
   Future<String?> _uploadFile({
     required File file,
     required String messageId,
     required String path,
   }) async {
-    final Reference reference = storage.ref(path + messageId);
+    final Reference reference = _storage.ref(path + messageId);
     await reference.putFile(file);
 
     final String? url = await reference.getDownloadURL();
