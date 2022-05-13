67d772365880a604774957c4d7045993d6c5730b
Harpreet Sangar <harpreet.sangar@applify.co>
2022-05-13 09:45:54 +0530

    refact: Make properties private.

--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -2,6 +2,15 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationService {
   int _notificationId = 0;
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  final NotificationDetails _notificationDetails = const NotificationDetails(
+    android: AndroidNotificationDetails(
+      'Flutter Library',
+      'Flutter Library',
+      importance: Importance.max,
+    ),
+  );
 
   Future<bool?> initialize() {
     const InitializationSettings initializationSettings =
@@ -12,23 +21,13 @@ class LocalNotificationService {
     return _flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
-  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
   void showLocalNotification(
       {String? title, String? body, String? payload}) async {
-    const NotificationDetails notificationDetails = NotificationDetails(
-        android: AndroidNotificationDetails(
-      'Flutter Library',
-      'Flutter Library',
-      importance: Importance.max,
-    ));
-
     await _flutterLocalNotificationsPlugin.show(
       _notificationId++,
       title,
       body,
-      notificationDetails,
+      _notificationDetails,
     );
   }
 }
